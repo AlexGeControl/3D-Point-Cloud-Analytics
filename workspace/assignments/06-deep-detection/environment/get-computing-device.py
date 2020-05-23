0f522a7ee8e6a9c7f295a04f320c66151df4b5de
Yao Ge <alexgecontrol@qq.com>
2020-05-24 00:10:05 +0800

    add point pillar in PyTorch.

---
+++ workspace/assignments/06-deep-detection/environment/get-computing-device.py
@@ -0,0 +1,14 @@
+#!/opt/conda/envs/deep-detection/bin/python
+import torch
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
